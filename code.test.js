0b03cb982bf1d9c42db504460d5525b264ea02bf
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-12 23:41:01 -0600

    Update code.test.js from mergesort test

--- code.test.js
+++ code.test.js
@@ -1,23 +1,15 @@
-const { parallelMergeSort } = require('./parallelMergeSort');
+const fs = require('fs');
+const jsc = require('jsverify');
 
-async function runTests() {
-    const data = Array.from({ length: 100 }, () => Math.floor(Math.random() * 1000));
-    console.log('Original:', data);
+eval(fs.readFileSync('code.js')+'');
 
-    const parallelMergeSortedData = await parallelMergeSort(data);
-    console.log('parallelMergeSorted:', parallelMergeSortedData);
+const testparallelMergeSort =
+    jsc.forall("array nat", function(arr) {
+        var a1 = JSON.parse(JSON.stringify(arr));
+        var a2 = JSON.parse(JSON.stringify(arr));
+        return JSON.stringify(parallelMergeSort(a1)) ==
+            JSON.stringify(a2.sort(function(a, b)
+                { return a - b; }));
+    });
 
-    const expected = [...data].parallelMergeSort((a, b) => a - b);
-    console.assert(
-        JSON.stringify(parallelMergeSortedData) === JSON.stringify(expected),
-        'Test failed: parallelMergeSorting does not match expected result'
-    );
-
-    if (JSON.stringify(parallelMergeSortedData) === JSON.stringify(expected)) {
-        console.log('Test passed!');
-    } else {
-        console.error('Test failed!');
-    }
-}
-
-runTests().catch(console.error);
+jsc.assert(testparallelMergeSort);
